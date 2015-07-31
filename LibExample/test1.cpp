05c425beaae02b58b4f81b482a45c1af98a09287
jszhujun2010 <jszhujun2010@gmail.com>
2015-07-31 18:43:40 +0800

    add some comments

--- LibExample/test1.cpp
+++ LibExample/test1.cpp
@@ -61,38 +61,52 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// CompileInstance will hold the instance of Clang compiler for us,
+	// managing the various objects needed to run the compiler.
 	CompilerInstance Inst;
+
+	// Diagnostics manage problems and issues in compile
 	Inst.createDiagnostics(NULL, false);
 
-	//TargetOptions *TO = new TargetOptions();
-	//Note that shared pointer is used here.
+	// TargetOptions *TO = new TargetOptions();
+	// Note that shared pointer is used here.
+	// Set target paltform options, initi target info with default triple for our platform.
 	std::shared_ptr<clang::TargetOptions> TO = std::make_shared<clang::TargetOptions>();
 	TO->Triple = llvm::sys::getDefaultTargetTriple();
 	TargetInfo *TI = TargetInfo::CreateTargetInfo(Inst.getDiagnostics(), TO);
 	Inst.setTarget(TI);
 
+	// FileManager supports for file system lookup, caching and directory search management.
 	Inst.createFileManager();
 	FileManager &FileMgr = Inst.getFileManager();
 
+	// SourceManager handles loading and caching of source files into memory.
 	Inst.createSourceManager(FileMgr);
 	SourceManager &SourceMgr = Inst.getSourceManager();
 
+	// Preprocessor runs within a single file.
 	Inst.createPreprocessor(clang::TU_Complete);
 
+	// ASTContext holds long-lived AST nodes (such as types and decls)
 	Inst.createASTContext();
 
+	// A Rewriter helps us manage the code rewriting task.
 	Rewriter TheRewriter;
 
 	TheRewriter.setSourceMgr(SourceMgr, Inst.getLangOpts());
 
+	// Set the main file handled by the source manager to the input file.
 	const FileEntry *FileIn = FileMgr.getFile(argv[1]);
-	//Seems new API is used.
+	// New API is used.
 	SourceMgr.setMainFileID(SourceMgr.createFileID(FileIn, clang::SourceLocation(), clang::SrcMgr::C_User));
 
+	// Inform Diagnostics that processing of a source file is begining.
 	Inst.getDiagnosticClient().BeginSourceFile(Inst.getLangOpts(), &Inst.getPreprocessor());
 
+	// Create an AST consumer instance which is gong to be called by ParseAST.
 	MyASTConsumer TheConsumer;
 
+	// Parse the file to AST, registering our consumer as the ASTConsumer.
 	ParseAST(Inst.getPreprocessor(), &TheConsumer, Inst.getASTContext());
 
 	return 0;
